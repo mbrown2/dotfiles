714c6722005cb0dd6f4fe1de6368635dfc8e2dd2
mbrown2 <17942160+mbrown2@users.noreply.github.com>
2025-10-13 09:54:50 -0700

    Initial config

---
+++ dot_config/zsh/dot_zshrc.d/aliases.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+#
+# .aliases - Set whatever shell aliases you want.
+#
+
+# mask vi
+alias vi=vim
+
+# more ways to ls
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+# fix typos
+alias cd..='cd ..'
+
+# misc
+alias cm=chezmoi
+alias please=sudo
+alias zshrc='$EDITOR $ZDOTDIR/.zshrc'
+alias zdot='cd $ZDOTDIR'
